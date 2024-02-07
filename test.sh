6efa73cc01d118cf52a8f9640395504b875b7fdb
lez <leeinsteinzhang@gmail.com>
2024-02-07 10:14:13 -0500

    fix typos

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ script_path=$(realpath "$0")
 script_dir=$(dirname "$script_path")
 
 # 计划任务（Cron Jobs）定义
-palword_joobs="# PalWorld Jobs"
+palword_jobs="# PalWorld Jobs"
 backup_job="*/15 * * * * $script_dir/backup.sh"
 backup_daily_job="0 6 * * * $script_dir/backup_daily.sh"
 backup_monthly_job="0 0 1 * * $script_dir/backup_monthly.sh"
@@ -19,7 +19,7 @@ add_cron_job_if_not_exists() {
 }
 
 # 添加每个计划任务
-add_cron_job_if_not_exists "$palword_joobs"
+add_cron_job_if_not_exists "$palword_jobs"
 add_cron_job_if_not_exists "$backup_job"
 add_cron_job_if_not_exists "$backup_daily_job"
 add_cron_job_if_not_exists "$backup_monthly_job"
