1b583a41ea1b2f89df8f42ccc57057edf36db3de
lez <leeinsteinzhang@gmail.com>
2024-02-07 10:08:09 -0500

    #

--- test.sh
+++ test.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 
-# 备份保存时长
-n=7
-
 # 获取当前脚本的完整路径
 script_path=$(realpath "$0")
 script_dir=$(dirname "$script_path")
 
-# 定位到备份目录的路径，假设备份目录位于脚本目录的上一级目录中的Backup文件夹
-backup_dir="$script_dir/../Pal/Backup"
-backup_dir=$(realpath "$backup_dir")
-mkdir -p "${backup_dir}"
+# 计划任务（Cron Jobs）定义
+palword_joobs="# Palword Settings"
+backup_job="*/15 * * * * $script_dir/backup.sh"
+backup_daily_job="0 6 * * * $script_dir/backup_daily.sh"
+backup_monthly_job="0 0 1 * * $script_dir/backup_monthly.sh"
+reboot_job="10 6 * * * $script_dir/reboot.sh"
+watchdog_job="*/5 * * * * $script_dir/watchdog.sh"
 
-saved_dir="$script_dir/../Pal"
-saved_dir=$(realpath "$saved_dir")
+# 检查并添加每个cron作业
+add_cron_job_if_not_exists() {
+    local job="$1"
+    (crontab -l 2>/dev/null | grep -Fq -- "$job") || (crontab -l 2>/dev/null; echo "$job") | crontab -
+}
 
-# 获取时间
-current_date=$(date +%Y%m%d)
-current_hour=$(date +%H%M)
-
-# 为当前日期创建备份目录
-mkdir -p "${backup_dir}/${current_date}"
-
-# # 在Saved目录中创建备份存档
-# cd "$saved_dir" && zip -r "${backup_dir}/${current_date}/backup_${current_date}_${current_hour}.zip" ./Saved/
-
-# 计算n天前的日期
-n_days_ago=$(date -d "-${n} days" +%Y%m%d)
-
-# 删除n天前的所有备份目录及其内容
-find "${backup_dir}" -mindepth 1 -maxdepth 1 -type d -name "????????" | while read dir; do
-    dir_name=$(basename "$dir")
-    echo "$dir_name"
-done
+# 添加每个计划任务
+add_cron_job_if_not_exists "$palword_joobs"
+add_cron_job_if_not_exists "$backup_job"
+add_cron_job_if_not_exists "$backup_daily_job"
+add_cron_job_if_not_exists "$backup_monthly_job"
+add_cron_job_if_not_exists "$reboot_job"
+add_cron_job_if_not_exists "$watchdog_job"
